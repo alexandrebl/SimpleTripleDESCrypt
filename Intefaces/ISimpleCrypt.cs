7bfd383489e39cf8ff3aa62179e989cc7176fd4d
Alexandre Brandão Lustosa <alexandre.brandao.lustosa@gmail.com>
2017-05-08 19:51:42 -0300

    First commit

---
+++ Intefaces/ISimpleCrypt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTripleDESCrypt.Intefaces {
+
+    /// <summary>
+    /// Simple crypto library
+    /// </summary>
+    public interface ISimpleCrypt {
+
+        /// <summary>
+        /// Encrypt data
+        /// </summary>
+        /// <param name="data">data to encrypt</param>
+        /// <returns>result of encrytation</returns>
+        byte[] TripleDesEncrypt(byte[] data);
+
+        /// <summary>
+        /// Decrypt data
+        /// </summary>
+        /// <param name="data">data to decrypt</param>
+        /// <returns>result of decryptation</returns>
+        byte[] TripleDesDecrypt(byte[] data);
+    }
+}
---
+++ Library/SimpleCrypt.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography;
+using SimpleTripleDESCrypt.Intefaces;
+
+namespace SimpleTripleDESCrypt.Library {
+
+    /// <summary>
+    /// Simple crypto library
+    /// </summary>
+    public class SimpleCrypt : ISimpleCrypt {
+
+        /// <summary>
+        /// TripleDES library
+        /// </summary>
+        private readonly TripleDES _tripleDes;
+
+        /// <summary>
+        /// Contructor method
+        /// </summary>
+        /// <param name="desKey">3 DES Key </param>
+        /// <param name="desIv">Component</param>
+        /// <param name="paddingMode">Padding mode</param>
+        /// <param name="cipherMode">cipher mode</param>
+        public SimpleCrypt(byte[] desKey, byte[] desIv, PaddingMode paddingMode = PaddingMode.None, CipherMode cipherMode = CipherMode.CBC) {
+            //Initialize library
+            _tripleDes = new TripleDESCryptoServiceProvider {
+                Key =  desKey,
+                IV = desIv,
+                Padding = paddingMode,
+                Mode = cipherMode
+            };
+        }
+
+        /// <summary>
+        /// Encrypt data
+        /// </summary>
+        /// <param name="data">data to encrypt</param>
+        /// <returns>result of encrytation</returns>
+        public byte[] TripleDesEncrypt(byte[] data) {
+            //Create crypto library
+            var ct = _tripleDes.CreateEncryptor();
+            //Output data
+            var output = ct.TransformFinalBlock(data, 0, data.Length);
+            //Result
+            return output;
+        }
+
+        /// <summary>
+        /// Decrypt data
+        /// </summary>
+        /// <param name="data">data to decrypt</param>
+        /// <returns>result of decryptation</returns>
+        public byte[] TripleDesDecrypt(byte[] data) {
+            //Create decrypto library
+            var ct = _tripleDes.CreateDecryptor();
+            //Output data
+            var output = ct.TransformFinalBlock(data, 0, data.Length);
+            //Result
+            return output;
+        }
+    }
+}
